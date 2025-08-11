5c58ec1a39e990ec346de4f0ef2b62fb31875768
Erkigmo <aaronsgale@gmail.com>
2025-08-11 18:56:21 -0400

    Get locations

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/parts/location/location.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/parts/location/location.java
@@ -1,11 +1,35 @@
 package org.firstinspires.ftc.teamcode.robot.parts.location;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
 import org.firstinspires.ftc.teamcode.libs.Point;
 
 public class location {
+    private final LinearOpMode linearOpMode;
 
+    double angle;
+    Point location;
+
+    imu imu;
+    odo odo;
+
+    public location(LinearOpMode LOM) {
+        linearOpMode = LOM;
+        imu = new imu(LOM);
+        odo = new odo(LOM);
+    }
+
+    public void init() {
+        imu.init();
+        odo.init();
+    }
 
     public Point getLocation() {
-        return new Point(4, 12);
+        location = odo.getPos();
+        return location;
+    }
+    public double getRotation() {
+        angle = imu.getYaw();
+        return angle;
     }
 }
