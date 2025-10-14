c74452fffdb3b348b424b880ea80c730fef21d36
Erkigmo <aaronsgale@gmail.com>
2025-10-14 17:57:10 -0400

    Lifts

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/robot/lifts/doubleLift.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode.core.robot.lifts;
+
+import dev.nextftc.control.ControlSystem;
+import dev.nextftc.core.commands.Command;
+import dev.nextftc.core.subsystems.Subsystem;
+import dev.nextftc.hardware.controllable.RunToPosition;
+import dev.nextftc.hardware.impl.MotorEx;
+
+public class doubleLift implements Subsystem {
+    public static final doubleLift INSTANCE = new doubleLift();
+    private doubleLift() { }
+
+    private final MotorEx right_motor = new MotorEx("right_lift_motor");
+    private final MotorEx left_motor = new MotorEx("left_lift_motor");
+
+    private final ControlSystem controlSystem = ControlSystem.builder()
+            .posPid(0.005, 0, 0)
+            .elevatorFF(0)
+            .build();
+
+    public Command toLow = new RunToPosition(controlSystem, 0).requires(this);
+    public Command toMiddle = new RunToPosition(controlSystem, 500).requires(this);
+    public Command toHigh = new RunToPosition(controlSystem, 1200).requires(this);
+
+    @Override
+    public void periodic() {
+        right_motor.setPower(controlSystem.calculate(right_motor.getState()));
+        left_motor.setPower(controlSystem.calculate(left_motor.getState()));
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/robot/lifts/singleLift.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode.core.robot.lifts;
+
+import dev.nextftc.control.ControlSystem;
+import dev.nextftc.core.commands.Command;
+import dev.nextftc.core.subsystems.Subsystem;
+import dev.nextftc.hardware.controllable.RunToPosition;
+import dev.nextftc.hardware.impl.MotorEx;
+
+public class singleLift implements Subsystem {
+    public static final singleLift INSTANCE = new singleLift();
+    private singleLift() { }
+
+    private final MotorEx motor = new MotorEx("lift_motor");
+
+    private final ControlSystem controlSystem = ControlSystem.builder()
+            .posPid(0.005, 0, 0)
+            .elevatorFF(0)
+            .build();
+
+    public Command toLow = new RunToPosition(controlSystem, 0).requires(this);
+    public Command toMiddle = new RunToPosition(controlSystem, 500).requires(this);
+    public Command toHigh = new RunToPosition(controlSystem, 1200).requires(this);
+
+    @Override
+    public void periodic() {
+        motor.setPower(controlSystem.calculate(motor.getState()));
+    }
+}
