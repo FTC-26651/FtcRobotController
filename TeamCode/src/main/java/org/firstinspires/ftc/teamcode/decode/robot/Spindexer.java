5f062871d8947fb430677f8c890756dd1d0b07ad
Erkigmo <aaronsgale@gmail.com>
2025-10-18 12:43:06 -0400

    Work on Spindexer

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/decode/robot/Spindexer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/decode/robot/Spindexer.java
@@ -6,6 +6,8 @@ import dev.nextftc.core.subsystems.Subsystem;
 import dev.nextftc.core.units.Angle;
 import dev.nextftc.hardware.impl.ServoEx;
 
+// When I was writing this, only I and God knew what this does. Now only God knows what it does
+// Jk it's not that bad, but it is the most complicated thing I've written so far
 public class Spindexer implements Subsystem {
     public static final Spindexer INSTANCE = new Spindexer();
     private Spindexer() { }
@@ -14,6 +16,7 @@ public class Spindexer implements Subsystem {
         private final Angle thirdCircle = Angle.fromRev(1 / 3);
 
         private final int INDEX_SIZE = 3;
+        private final int PATTERN_SIZE = 3;
 
         private final int inputLocation = 0;
         private final int outputLocation = 0;
@@ -21,10 +24,42 @@ public class Spindexer implements Subsystem {
         private final String name = "spindex_servo";
     }
 
-    public Spindexer.Params PARAMS = new Spindexer.Params();
+    private final Params PARAMS = new Params();
 
     private final ServoEx spindexMotor = new ServoEx(PARAMS.name);
+
     private final String[] INDEX = new String[PARAMS.INDEX_SIZE];
+    private String[] PATTERN = new String[PARAMS.PATTERN_SIZE];
+    private int patternIncrement = 0;
+
+    private int getIndexOf(String color) {
+        for (int i = 0;  i < PARAMS.INDEX_SIZE; i++) {
+            if (color.equals(INDEX[i])) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private String getOrbToShoot() {
+        patternIncrement++;
+        return PATTERN[patternIncrement];
+    }
+
+    private void setOrbToShoot(String color) {
+        int indexOfColor = getIndexOf(color);
+        while (indexOfColor != PARAMS.outputLocation) {
+            if (indexOfColor > PARAMS.outputLocation) {
+                INSTANCE.rotateLeft.schedule();
+            }
+            if (indexOfColor < PARAMS.outputLocation) {
+                INSTANCE.rotateRight.schedule();
+            }
+            indexOfColor = getIndexOf(color);
+        }
+    }
+
+
 
     public Command rotateLeft = new InstantCommand(() -> {
         spindexMotor.setPosition(spindexMotor.getPosition() + PARAMS.thirdCircle.inRev);
@@ -50,10 +85,6 @@ public class Spindexer implements Subsystem {
         return new InstantCommand(() -> INDEX[PARAMS.outputLocation] = "");
     }
 
-    public String getOrbColor(int pos) {
-        return INDEX[pos];
-    }
-
     public boolean isFull() {
         for (String color : INDEX) {
             if (color.isEmpty()) {
@@ -62,4 +93,14 @@ public class Spindexer implements Subsystem {
         }
         return true;
     }
+
+    public void setPattern(String[] pattern) {
+        if (pattern.length == PARAMS.PATTERN_SIZE) {
+            PATTERN = pattern;
+        }
+    }
+
+    public Command index = new InstantCommand(() -> {
+        setOrbToShoot(getOrbToShoot());
+    });
 }
