0fccf1789fbd7d11f58b8aae5049680135cd0564
Erkigmo <aaronsgale@gmail.com>
2025-12-06 09:20:57 -0500

    Add transfer

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/robot/transfers/wheels/SingleMotorTransfer.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.core.robot.transfers.wheels;
+
+import dev.nextftc.control.ControlSystem;
+import dev.nextftc.core.commands.Command;
+import dev.nextftc.core.subsystems.Subsystem;
+import dev.nextftc.hardware.controllable.RunToVelocity;
+import dev.nextftc.hardware.impl.MotorEx;
+
+public class SingleMotorTransfer implements Subsystem {
+    public static final SingleMotorTransfer INSTANCE = new SingleMotorTransfer();
+    private SingleMotorTransfer() { }
+
+    private final MotorEx motor = new MotorEx("transfer_motor");
+
+    private final ControlSystem controller = ControlSystem.builder()
+            .velPid(0.005, 0, 0)
+            .basicFF(0.01, 0.02, 0.03)
+            .build();
+
+    public final Command off = new RunToVelocity(controller, 0.0).requires(this).named("TransferOff");
+    public final Command on = new RunToVelocity(controller, 500.0).requires(this).named("TransferOn");
+
+    @Override
+    public void periodic() {
+        motor.setPower(controller.calculate(motor.getState()));
+    }
+}
