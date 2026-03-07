38f0a7def6fa987d3fc8e012ba7adf3d75d911b8
Erkigmo <aaronsgale@gmail.com>
2026-03-06 21:03:32 -0500

    Work on pedro a bit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/robot/drivetrain/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/robot/drivetrain/Constants.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.core.robot.drivetrain;
 
+import com.pedropathing.control.FilteredPIDFCoefficients;
+import com.pedropathing.control.PIDFCoefficients;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.ftc.FollowerBuilder;
@@ -14,7 +16,12 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 public class Constants {
     public static FollowerConstants followerConstants = new FollowerConstants()
-            .mass(5);
+            .mass(10.7)
+            .forwardZeroPowerAcceleration(-23.865544747689892)
+            .lateralZeroPowerAcceleration(-27.378851268085214)
+            .translationalPIDFCoefficients(new PIDFCoefficients(0.09, 0.00005, 0.0001, 0))
+            .headingPIDFCoefficients(new PIDFCoefficients(0.9, 0.001, 0, 0.01))
+            .drivePIDFCoefficients(new FilteredPIDFCoefficients(0.025, 0.0, 0.00001, 0.6, 0.01));
 
     public static MecanumConstants driveConstants = new MecanumConstants()
             .maxPower(1)
@@ -25,7 +32,9 @@ public class Constants {
             .leftFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
             .leftRearMotorDirection(DcMotorSimple.Direction.FORWARD)
             .rightFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
-            .rightRearMotorDirection(DcMotorSimple.Direction.REVERSE);
+            .rightRearMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .xVelocity(67.44324163001355)
+            .yVelocity(58.295635073203734);
 
     public static PinpointConstants localizerConstants = new PinpointConstants()
             .forwardPodY(-5)
@@ -33,7 +42,7 @@ public class Constants {
             .distanceUnit(DistanceUnit.INCH)
             .hardwareMapName("pinpoint")
             .encoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD)
-            .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD)
+            .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.REVERSED)
             .strafeEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD);
 
     public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
