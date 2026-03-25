db90659fe34f59b45700d84197e4e9fb272aaa10
Erkigmo <aaronsgale@gmail.com>
2026-03-24 21:40:49 -0400

    Change double flywheel to use MotorGroup

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/robot/flywheels/DoubleFlywheel.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/robot/flywheels/DoubleFlywheel.java
@@ -1,8 +1,10 @@
 package org.firstinspires.ftc.teamcode.core.robot.flywheels;
 
 import dev.nextftc.control.ControlSystem;
+import dev.nextftc.control.KineticState;
 import dev.nextftc.core.commands.Command;
 import dev.nextftc.core.subsystems.Subsystem;
+import dev.nextftc.hardware.controllable.MotorGroup;
 import dev.nextftc.hardware.controllable.RunToVelocity;
 import dev.nextftc.hardware.impl.MotorEx;
 
@@ -10,20 +12,30 @@ public class DoubleFlywheel implements Subsystem {
     public static final DoubleFlywheel INSTANCE = new DoubleFlywheel();
     private DoubleFlywheel() { }
 
-    private final MotorEx rightMotor = new MotorEx("right_flywheel_motor");
-    private final MotorEx leftMotor = new MotorEx("left_flywheel_motor");
+    private ControlSystem controller;
+    private MotorGroup motors;
 
-    private final ControlSystem controller = ControlSystem.builder()
-            .velPid(0.005, 0, 0)
-            .basicFF(0.01, 0.02, 0.03)
-            .build();
+    public final Command off = new RunToVelocity(controller, 0.0).requires(this).named("Flywheel Off");
+    public final Command on = new RunToVelocity(controller, 500.0).requires(this).named("Flywheel On");
 
-    public final Command off = new RunToVelocity(controller, 0.0).requires(this).named("FlywheelOff");
-    public final Command on = new RunToVelocity(controller, 500.0).requires(this).named("FlywheelOn");
+    @Override
+    public void initialize() {
+        motors = new MotorGroup(
+                new MotorEx("right_flywheel_motor"),
+                new MotorEx("left_flywheel_motor")
+        );
+        controller = ControlSystem.builder()
+                .velPid(0.001, 0, 0)
+                .basicFF(0.003, 0.08, 0.00)
+                .build();
+        controller.setGoal(new KineticState(0.0, 0.0));
+    }
 
     @Override
     public void periodic() {
-        rightMotor.setPower(controller.calculate(rightMotor.getState()));
-        leftMotor.setPower(controller.calculate(leftMotor.getState()));
+        motors.setPower(controller.calculate(new KineticState(
+                motors.getCurrentPosition(),
+                motors.getVelocity()))
+        );
     }
 }
